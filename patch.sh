78db0be5e068a8729092a867b6b51ca1a0fdf7c1
王春 <wangchun0410@outlook.com>
2018-11-17 16:44:45 +0800

    quick publish

--- patch.sh
+++ patch.sh
@@ -3,3 +3,5 @@
 cp themes/patch/comments.pug themes/maupassant/layout/_partial/comments.pug
 
 cp themes/patch/gitment.browser.js themes/maupassant/source/js/gitment.browser.js
+
+cp -r source/img/* themes/maupassant/source/img/
