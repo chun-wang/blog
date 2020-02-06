9489477e14fd93cd64ca233c4f7ca3293dca728f
王春 <wangchun0410@outlook.com>
2020-02-06 21:37:16 +0800

    quick publish

--- patch.sh
+++ patch.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-cp themes/patch/comments.pug themes/maupassant/layout/_partial/comments.pug
-
-cp themes/patch/gitment.browser.js themes/maupassant/source/js/gitment.browser.js
-
-cp themes/patch/footer.pug themes/maupassant/layout/_partial/footer.pug
-
+cp -r themes/patch/* themes/maupassant/
 cp -r source/img/* themes/maupassant/source/img/
 
 
